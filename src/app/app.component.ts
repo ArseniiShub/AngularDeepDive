bdbb70523e19b50653a0ddbed197df20313bf871
ArseniiShub <arsenii.shub@sharp-dev.net>
2024-02-06 00:25:17 +0100

    Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,13 @@ import { Course } from './model/course';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   courses = COURSES;
+  startDate = new Date(2000, 0, 1);
+  title = COURSES[0].description;
+  price = 9.99123456;
+  rate = 0.67;
+  course = COURSES[0];
 
   onCourseSelected(course: Course) {
     console.log('App component - click event bubbled ...', course);
