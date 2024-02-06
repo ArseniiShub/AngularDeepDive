6463e947e667038507ac8649d082c149364a505d
ArseniiShub <arsenii.shub@sharp-dev.net>
2024-02-06 22:05:44 +0100

    ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { COURSES } from '../db-data';
 import { Course } from './model/course';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
   courses = COURSES;
-  startDate = new Date(2000, 0, 1);
-  title = COURSES[0].description;
-  price = 9.99123456;
-  rate = 0.67;
-  course = COURSES[0];
+
+  @ViewChild('cardRef', { read: ElementRef })
+  card: CourseCardComponent;
+
+  @ViewChild('courseImage')
+  courseImage: ElementRef;
+
+  constructor() {
+    console.log('containerDiv:', this.card);
+  }
+
+  ngAfterViewInit(): void {
+    console.log('containerDiv:', this.card);
+  }
 
   onCourseSelected(course: Course) {
-    console.log('App component - click event bubbled ...', course);
+    console.log('courseImage:', this.courseImage);
   }
 }
